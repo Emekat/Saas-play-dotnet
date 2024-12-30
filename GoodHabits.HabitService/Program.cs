614a818973f8503eba08839b1ad15a0be06abda1
Chukwuemeka Torti <emekatorti@gmail.com>
2024-12-30 22:01:39 +0000

    feat: wired up services

--- GoodHabits.HabitService/Program.cs
+++ GoodHabits.HabitService/Program.cs
@@ -1,18 +1,29 @@
+using GoodHabits.Database;
+using GoodHabits.Database.Interfaces;
+using GoodHabits.HabitService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddTransient<IHabitService, HabitService>();
+builder.Services.AddTransient<ITenantService, TenantService>();
+builder.Services.Configure<TenantSettings>(builder.Configuration.GetSection(nameof(TenantSettings)));
+builder.Services.AddAndMigrateDatabases(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
 }
 app.MapControllers();
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
