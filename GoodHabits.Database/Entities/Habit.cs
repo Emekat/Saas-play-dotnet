55a624146427a78d45f70a050193e079ed3e345b
Chukwuemeka Torti <emekatorti@gmail.com>
2024-12-30 11:50:25 +0000

    added tenant model

--- GoodHabits.Database/Entities/Habit.cs
+++ GoodHabits.Database/Entities/Habit.cs
@@ -1,11 +1,13 @@
-public record Habit
+public record Habit : IHasTenant
 {
     public int Id { get; set; }
  
     // Guid.CreateVersion7();
     public Guid AccountabilityPartnerId { get; set; } = Guid.Empty;
 
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
     public bool IsCompleted { get; set; }
+    public Guid TenantId { get; set; } = default;
+    public string TenantName { get; set; } = default!;
 }
