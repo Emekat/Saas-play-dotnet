1cca234d24f90186c5e167920171c88c61e4abaf
Chukwuemeka Torti <emekatorti@gmail.com>
2024-12-30 14:21:49 +0000

    feat: add TenantName to seeded Habit data

--- GoodHabits.Database/SeedData.cs
+++ GoodHabits.Database/SeedData.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 public static class SeedData{
     public static void Seed(ModelBuilder modelBuilder){
         modelBuilder.Entity<Habit>().HasData(
-            new Habit { Id = 1, Name = "Exercise", Description = "Do some exercise" },
-            new Habit { Id = 2, Name = "Read", Description = "Read a book" },
-            new Habit { Id = 3, Name = "Meditate", Description = "Meditate for 10 minutes" }
+            new Habit { Id = 1, Name = "Exercise", Description = "Do some exercise", TenantName = "CloudSphere" },
+            new Habit { Id = 2, Name = "Read", Description = "Read a book", TenantName = "CloudSphere" },
+            new Habit { Id = 3, Name = "Meditate", Description = "Meditate for 10 minutes", TenantName = "CloudSphere" }
         );
     }
 }
